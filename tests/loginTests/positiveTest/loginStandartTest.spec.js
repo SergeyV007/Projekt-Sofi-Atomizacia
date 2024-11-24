e686b5c4a8b3ba08627bb6e0f74d4e28824c7d64
Netanel Harush <netanelharush2@gmail.com>
2024-11-24 09:53:16 +0200

    Code Review

--- tests/loginTests/positiveTest/loginStandartTest.spec.js
+++ tests/loginTests/positiveTest/loginStandartTest.spec.js
@@ -1,24 +1,26 @@
-// import { test, expect } from "@playwright/test";
+import {test, expect} from '@playwright/test'
 
-// test.describe("Positve Login", () => {
-//   const baseURL = "https://www.saucedemo.com";
-//   const users = [
-//     { username: "standard_user", password: "secret_sauce" },
-//     { username: "problem_user", password: "secret_sauce" },
-//     { username: "performance_glitch_user", password: "secret_sauce" },
-//     { username: "error_user", password: "secret_sauce" },
-//     { username: "visual_user", password: "secret_sauce" },
-//   ];
+test.describe.skip('Positve Login', () => {
+    const baseURL = 'https://www.saucedemo.com'
+    const users = [
+        {username: 'standard_user', password: 'secret_sauce'},
+        {username: 'problem_user', password: 'secret_sauce'},
+        {username: 'performance_glitch_user', password: 'secret_sauce'},
+        {username: 'error_user', password: 'secret_sauce'},
+        {username: 'visual_user', password: 'secret_sauce'},
+    ]
 
-//   for (const user of users) {
-//     test(`Positive login test - ${user.username}`, async ({ page }) => {
-//       await page.goto(baseURL);
-//       await page.fill("#user-name", user.username);
-//       await page.fill("#password", user.password);
-//       await page.click("#login-button");
-//       await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
-//       const pageTitle = await page.locator(".title");
-//       await expect(pageTitle).toHaveText("Products");
-//     });
-//   }
-// });
+    for (const user of users) {
+        test(`Positive login test - ${user.username}`, async ({page}) => {
+            await page.goto(baseURL)
+            await page.fill('#user-name', user.username)
+            await page.fill('#password', user.password)
+            await page.click('#login-button')
+            await expect(page).toHaveURL(
+                'https://www.saucedemo.com/inventory.html',
+            )
+            const pageTitle = await page.locator('.title') // await is redundant here.
+            await expect(pageTitle).toHaveText('Products')
+        })
+    }
+})
