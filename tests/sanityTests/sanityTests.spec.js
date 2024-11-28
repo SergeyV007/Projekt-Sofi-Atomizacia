79fa30df3fbb95bd76b8a2dcba7d368506fdbb2e
Sergey Viakhirev <menstoundaimond@gmail.com>
2024-11-28 21:24:24 +0200

    new4.1

--- tests/sanityTests/sanityTests.spec.js
+++ tests/sanityTests/sanityTests.spec.js
@@ -1,6 +1,6 @@
 import {expect, test} from '@playwright/test'
 
-test.describe("test", () => {
+test.describe.only("test", () => {
   test("Verify Successful Purchase Process and Order Completion", async ({ page }) => {
     await page.goto("https://saucedemo.com");
   await expect(page.locator('[data-test="username"]')).toBeVisible();
@@ -11,21 +11,25 @@ test.describe("test", () => {
   await expect(page.locator('.title')).toHaveText("Products");
     await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
     await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
-    await page.locator('[data-test="shopping-cart-link"]').click();
-    await expect(page.locator('.cart_item')).toHaveCount(2); // this validation need to be before you enter the cart
-    // where are all of the validation on this pages i asked for? Page 13 in the presentation
+    await expect(page.locator('.shopping_cart_badge')).toHaveText("2");
+   await page.locator('[data-test="shopping-cart-link"]').click();
+    await expect(page.locator('.cart_item')).toHaveCount(2);
+     await expect(page.locator('.cart_item:has-text("Sauce Labs Backpack")')).toBeVisible();
+    await expect(page.locator('.cart_item:has-text("Sauce Labs Bike Light")')).toBeVisible();
     await page.locator('[data-test="checkout"]').click(); 
-  await expect(page.locator('[data-test="firstName"]')).toBeVisible(); // what is this validation
-  // where are all of the validation on this pages i asked for? Page 14 in the presentation
+  await expect(page.locator('[data-test="firstName"]')).toBeVisible();
+    await expect(page.locator('[data-test="lastName"]')).toBeVisible();
+    await expect(page.locator('[data-test="postalCode"]')).toBeVisible();
     await page.locator('[data-test="firstName"]').fill("Sergey");
     await page.locator('[data-test="lastName"]').fill("Viakhirev");
     await page.locator('[data-test="postalCode"]').fill("3288678");
     await page.locator('[data-test="continue"]').click();
-    // where are all of the validation on this pages i asked for? Page 15 in the presentation
+    await expect(page.locator('.summary_info')).toBeVisible();
+    await expect(page.locator('.cart_item')).toHaveCount(2);
     await page.locator('[data-test="finish"]').click();
     await expect(page.locator('[data-test="complete-header"]')).toHaveText(
-      "Thank you for your order!"
-    );
-    // where is the validation of the message description i asked for? Page 16 in the presentation
+      "Thank you for your order!" );
+    await expect(page.locator('.complete-text')).toHaveText(
+      "Your order has been dispatched, and will arrive just as fast as the pony can get there!");
   });
 });
